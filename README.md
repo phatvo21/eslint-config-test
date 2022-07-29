8f4db764f5d0991b860c39b50c860abbc26875f8
Phat Vo <phat@briohr.com>
2022-07-29 22:12:29 +0700

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Eslint typescript config backend
+
+# Install
+```npm i -D eslint-config-rolo-backend-ts eslint```
+
+Add it as a config in ```.eslintrc```
+```
+{
+  "extends": ["eslint-config-test"]
+}
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint/eslint-plugin"],
+    extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
+    root: true,
+    env: {
+        node: true,
+        jest: true
+    },
+    ignorePatterns: [".eslintrc.js", "**/dist/*", "**/coverage/*/", "**/node_modules/*"],
+    rules: {
+        "@typescript-eslint/ban-types": ["warn"],
+        "@typescript-eslint/explicit-member-accessibility": [
+            "warn",
+            {
+                accessibility: "explicit",
+                overrides: {
+                    accessors: "explicit",
+                    constructors: "off",
+                    methods: "explicit",
+                    properties: "explicit",
+                    parameterProperties: "explicit"
+                }
+            }
+        ],
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-explicit-any": "warn"
+    }
+};
